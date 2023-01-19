9214edf33d03a132297d37c411080aa23d91c063
teresabarker <teresa.barker12@gmail.com>
2023-01-19 13:23:12 -0500

    created main.js and added in info and comments syntax

---
+++ main.js
@@ -0,0 +1,11 @@
+/* 
+Title: main.js
+Description: The main file of my JavaScript Notes file 
+Purpose: Let me practice concepts and typey type things out so I learn them better
+Author: tbizzle
+Date: January 19, 2023
+*/
+
+/* This is a comment! */
+// this is also a comment. more casual tho
+
