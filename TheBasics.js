3a3453b3e99f2e50aaf61105f95e89ad7684cc8f
teresabarker <teresa.barker12@gmail.com>
2023-01-30 22:22:46 -0500

    reformatted the comments section; added in notes on data types

--- TheBasics.js
+++ TheBasics.js
@@ -6,15 +6,57 @@ Author: tbizzle
 Date: January 19, 2023
 */
 
-/* This is a comment! */
+/* 
+This is a comment! 
+*/
+
 // this is also a comment. more casual tho
 
-/* Console */
-// This section is me practicing console.log! 
-// console.log() is how to print something in JavaScript
+// best practice is to use this for single line comments
+// can use the multi-line comment to comment something out in the /*middle*/ of the line too
 
-// Priting my age
-console.log(24);
+/* Console 
+This section is me practicing console.log! 
+console.log() is how to print something in JavaScript
+*/
 
-// Printing my age, but better
-console.log("I am " + 24 + " years old");
+console.log(24); // Priting my age
+console.log("I am " + 24 + " years old"); // Printing my age, but better
+
+/* Data Types
+What's a data type you may ask? great question. a data type is how we classify or describe the various types of data used to program.
+Usually using the word in its own definition is frowned upon, but here we are.
+
+In JavaScript, there are 7 Fundamental Data Types (<- memorize that, Teresa)
+
+1) Number
+    this is any number. including those with decimals
+    4, 9, 1,000,000, .0000000012
+
+2) String
+    fancy word for a string of text. any string of text contained within single or double quotes
+    we JavaScript people tend to prefer single quotes '...' over double quotes "..."
+    numbers, spaces, letters, characters, etc. are all permissable
+    'my name is Terry', '5, 4, 3, 2, 1...', '!', ":("
+
+3) Boolean
+    true or false. no quotes. short and sweet. like a light switch. takes me right back to the Kresch days...
+    learningJavaScript = true, wantsToPlayCallOfDuty = false (a lie, shh)
+
+4) Null
+    null keyword. the *intentional* absence of a value. not nothing, not zero.../null/
+    thoughts = null;
+
+5) Undefined
+    undefined keyword. also represents the absence of a value, although not to be confused with null. 
+    undefined means a value does not exist.
+    I like to think of undefined as agnostic, and null as a no-thoughts-zen-state
+
+6) Sybmbol
+    a new feature to JavaScript. first impression, not good
+    symbols are unique identifiers, useful in complex coding. more of this later...
+
+7) Object
+    saving the best for last, objects are collections of data
+
+*/
