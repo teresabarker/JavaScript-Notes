63ca08e83cded8bf223b97529fd81a57513c1765
teresabarker <teresa.barker12@gmail.com>
2023-06-26 19:21:51 -0400

    fixed fizzbuzz

--- Practice/FizzBuzz.js
+++ Practice/FizzBuzz.js
@@ -11,13 +11,13 @@
 
 
 // here goes
-for (let n = 0; n < 10; n++) {
+for (let n = 0; n < 100; n++) {
     if ((n % 3 === 0) && (n % 5 === 0)) {
-        console.log("Fizz");
-    } else if (n % 3 === 0) {
-        console.log("Buzz");
-    } else if (n % 5 === 0) {
         console.log("FizzBuzz");
+    } else if (n % 3 === 0) {
+        console.log("Fizz");
+    } else if (n % 5 === 0) {
+        console.log("Buzz");
     } else {
         console.log(n);
     }
